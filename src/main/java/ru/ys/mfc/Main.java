f6e8c08837e8df94e64da150ed73199cc1cf450c
Yuriy Yeliseyev <yuriye@mail.ru>
2019-08-08 16:49:14 +1200

    190808.1

--- src/main/java/ru/ys/mfc/Main.java
+++ src/main/java/ru/ys/mfc/Main.java
@@ -12,12 +12,18 @@ public class Main {
 
         QuestionsFactory questionsFactory;
 
-        if (args.length > 0)
-            questionsFactory = QuestionsFactoryFactory.getQuestionsFactory(args[0]);
+        if (args.length > 0 && "m".equals(args[0]))
+            questionsFactory = QuestionsFactoryFactory.getQuestionsFactory("ru.ys.mfc.model.MockQuestions");
         else
-            questionsFactory = QuestionsFactoryFactory.getQuestionsFactory("MKGUQuestions");
+            questionsFactory = QuestionsFactoryFactory.getQuestionsFactory("ru.ys.mfc.model.MKGUQuestions");
 
         List<Question> questions = questionsFactory.getQuestions();
+        questions.stream().forEach(c -> {
+            System.out.println(c.getTitle() + ":" + c.getDescription());
+            c.getCandidates().stream().forEach(c1 -> System.out.println("\n\t" + c1.getId()
+                    + ":" + c1.getTitle()
+                    + ":" + c1.getAltTitle()));
+        });
 
     }
 }
---
+++ src/main/java/ru/ys/mfc/view/ProgressForm.java
@@ -0,0 +1,4 @@
+package ru.ys.mfc.view;
+
+public class ProgressForm {
+}
